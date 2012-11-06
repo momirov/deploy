bdf95463c6f53cbec555ef7dd79aac8376675945
Vladimir Momirov <vladimir.momirov@saturized.com>
2012-11-06 13:54:05 +0100

    Added production configs for ldap and postgresql.
    Change-Id: Iaae2b68ade0163c2083c0ecb711516372be3e8b5

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 
@@ -48,3 +48,7 @@ gem 'celluloid'
 
 gem 'private_pub'
 gem 'thin'
+
+gem 'foreman'
+
+gem 'pg'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :ldap_authenticatable, :rememberable, :trackable
-
+  
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :login, :password, :password_confirmation, :remember_me
+  attr_accessible :login, :password, :password_confirmation, :remember_me, :email
   # attr_accessible :title, :body
+
+  before_save :get_ldap_email
+
+  def get_ldap_email
+    self.email = "#{self.login}@saturized.com"
+  end
 end
