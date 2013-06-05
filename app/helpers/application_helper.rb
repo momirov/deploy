c3f60ed91a4e9525d6e7b72786c96fe0ce6a5cb4
Vladimir Momirov <vladimir.momirov@saturized.com>
2013-06-05 14:27:51 +0200

    Linked gerrit change ids.
    Change-Id: Iee37ea5fea51e859ef91288e2fe721d6b259c4e1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,15 @@ module ApplicationHelper
   end
 
   def base64_image(image)
-    ActiveSupport::Base64.encode64(image)
+    Base64.encode64(image)
   end
 
   def embed_gravatar(email)
     base64_image(Gravatar.new(email).image_data(:d => 'mm'))
+  end
+
+  def format_message(message)
+    html = simple_format(message)
+    html.gsub(/\b(Change-Id:\s*)([A-Za-z0-9]*)\b/, '\1' + link_to('\2', 'http://gerrit.saturized.com/r/\2'))
   end
 end
