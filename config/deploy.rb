f3641a99bca9b60847b3b0618a9b4d6a0d4a821e
Vladimir Momirov <vladimir.momirov@saturized.com>
2014-08-27 17:55:20 +0200

    Change shared folder for assets.
    Change-Id: Ica5c0f50ebc58c455a6c18a8d6c7adeb3246540d

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_to, "/var/www/deploy.saturized.com"
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/assets}
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
